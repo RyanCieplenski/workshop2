03e5d71fe9780bd3aadca60ef9f40f5eec4668a8
RyanCieplenski <137128200+RyanCieplenski@users.noreply.github.com>
2023-07-04 13:09:14 -0400

    first commit

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -131,7 +131,7 @@ else:
     STATIC_ROOT = [os.path.join(BASE_DIR,"static")]
 STATIC_URL = '/static/'
 
-# Default primary key field type
+# Default primary key field type:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
